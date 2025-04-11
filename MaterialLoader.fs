5b665f6f2a4ee8d0fe567b9852f6e1bd285645e5
Shandon Bowden <52051023+shandonb@users.noreply.github.com>
2025-04-11 17:39:11 -0400

    Cleaned up codebase

---
+++ MaterialLoader.fs
@@ -0,0 +1,27 @@
+﻿module MaterialLoader
+
+open System
+open System.IO
+open xivModdingFramework.Materials.FileTypes
+open xivModdingFramework.Materials.DataContainers
+open xivModdingFramework.Mods
+open xivModdingFramework.Textures.Enums
+open xivModdingFramework.Textures.FileTypes
+
+type LoadedTexture =
+    {
+        Usage: XivTexType
+        Path: string
+        Data: byte[]
+        Width: int
+        Height: int
+    }
+
+type LoadedMaterial =
+    {
+        MaterialPath: string
+        Textures: LoadedTexture list
+        ColorSet: byte[] option
+        Flags: Map<string, obj>
+    }
+
